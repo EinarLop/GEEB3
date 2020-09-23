3a9d2aae0feadbc747e99fcb8e28f25d1246bfcb
Einar <einar@pop-os.localdomain>
2020-09-23 14:36:09 -0500

    Sass

---
+++ frontend/src/CreateProject/createProject.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './createProjectStyles.scss'
+class CreateProject extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+        <div class='test'>
+         Hola
+        </div> );
+    }
+}
+ 
+export default CreateProject;
