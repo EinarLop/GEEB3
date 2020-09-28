0583cd43daa882f51a75061311e7706d03370452
Eric Chao <ericjardon@hotmail.com>
2020-09-27 22:07:20 -0500

    Some work done on Profile Interface. Grid, general styles, sample avatar.

--- frontend/src/PublicProfile/publicProfile.js
+++ frontend/src/PublicProfile/publicProfile.js
@@ -1,10 +1,32 @@
 import React from 'react'
 import './publicProfile.scss'
+import Header from './components/Header.js';
 
 export default function publicProfile() {
     return (
         <div>
-            <button class="editBtn">Edit Info</button>
+            <Header/>
+
+            <div class="grid-wrapper">
+                <div class="profile-sidebar">
+                    <img className= "profile-pic" src="https://www.w3schools.com/howto/img_avatar2.png" alt="Avatar"/>
+                    <div className="profile-name">Profile name</div>
+                    <div className="profile-desc">
+                        <p>Profesora de tiempo completo en el Tec de Monterrey
+                        Campus Ciudad de México. Más de 20 años de experiencia 
+                        desarrollando software. Me encantan los gatos.
+                        </p>
+                    </div>
+                    <div className="profile-stats">Major: ITC | Semester: 5 | Leading: 1 project | Contributing: 2 projects</div>
+                </div>
+
+
+                <div class="profile-content"><h3>Profile content</h3>            
+                    <button class="edit-btn">Edit Info</button>
+                </div>
+
+            </div>
         </div>
+        
     )
 }
