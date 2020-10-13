b8c99ec9f9e46df03c605b8c1d670b883a834140
Bryan Monroy <a01026848@itesm.mx>
2020-10-12 19:18:20 -0500

    Login and register

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 import CreateProject from './CreateProject/createProject.js'
 import PublicProfile from './PublicProfile/publicProfile.js'
+import Register from './Registration/register'
+import Login from './Login/login'
 
 function App() {
   return (
-    <div>
-    <PublicProfile/>
-    </div>
+    <Register/>
   );
 }
 
---
+++ frontend/src/Registration/register.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import './register.scss'
+
+export default function login() {
+    return (
+        <body>
+
+            <div className="Information">
+                <h1>What is GEEB?</h1>
+                <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae
+ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris</p>
+            </div>
+
+            <div className="Inputs">
+                <h1>Register now</h1>
+                <input placeholder="Placeholder"></input>
+                <input placeholder="Placeholder"></input>
+                <input placeholder="Placeholder"></input>
+                <input placeholder="Placeholder"></input>
+                <input placeholder="Placeholder"></input>
+                <button>Register</button>
+
+                <h3>Do you already have an account?</h3>
+                <a href="login">Login</a>
+            </div>
+
+        </body>
+    )
+}
