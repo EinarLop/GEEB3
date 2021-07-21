11bff5232cfe08727fb8a74d5dfb0621d501c0f9
Eric Chao <ericjardon@hotmail.com>
2021-07-20 23:10:51 -0500

    start work on firebase auth, add useLogin hook

--- src/TestAuth.js
+++ src/TestAuth.js
@@ -14,15 +14,20 @@ export default function TestAuth() {
         setPassword(e.target.value);
     }
 
-    const onSubmit = async () => {
-        const res = await auth.createUserWithEmailAndPassword(email, password);
+    const registerUser = async () => {
+        // Returns a user's credentials
+        const userCredential = await auth.createUserWithEmailAndPassword(email, password);
+    }
+    const loginUser = async () => {
+        // 
+        const userCredential = await auth.signInWithEmailAndPassword(email, password);
     }
 
     return (
         <div style={{ border: '2px blue solid', display: 'flex', flexDirection: 'column' }}>
             <input onChange={handleEmail} style={{ height: '48px', fontSize: '22px', maxWidth: '400px' }} />
             <input onChange={handlePassword} style={{ height: '48px', fontSize: '22px', maxWidth: '400px' }} />
-            <button onClick={onSubmit} style={{ width: '56', height: '32px' }}>Submit</button>
+            <button onClick={registerUser} style={{ width: '56', height: '32px' }}>Submit</button>
         </div>
     )
 }
