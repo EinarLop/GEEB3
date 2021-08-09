7289cbf3519d55d2c6682c973329571acd4d384b
einar <einarlop0@gmail.com>
2021-08-08 21:18:05 -0500

    Feedback email sent

--- src/EmailTest/EmailTest.jsx
+++ src/EmailTest/EmailTest.jsx
@@ -1,19 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from "axios"
 import styles from './EmailTestStyles.module.scss'
+import { Redirect, Link } from "react-router-dom";
 
 
 
 
 
 const EmailTest = (props) => {
+  const [applicants, setApplicants] = useState([])
+  const [status, setStatus] = useState()
+  const [redirect, setRedirect] = useState(false);
+
+  useEffect(() => {
+    axios
+      .post("http://localhost:3010/email/applicants", { oprojectid: props.match.params.id }) ///"http://localhost:3010/oprojects" http://localhost:3010oprojects
+      .then((response) => setApplicants(response.data))
+      .catch((error) => console.log(error));
+  }, []);
 
   const [mail, setMail] = useState({
     subject: "",
     text: "",
     oprojectid: props.match.params.id,
   })
-
   const handleOnChange = (e) => {
     setMail({
       ...mail,
@@ -25,15 +35,49 @@ const EmailTest = (props) => {
   const sendEmail = () => {
     axios.post("http://localhost:3010/email", mail)
       .then((response) => {
-        console.log("Wooo")
+        let msg = (
+          <p className={`${styles.StatusMsg} ${styles.Ok}`}> The email was sent successfully </p>
+        )
+        setStatus(msg)
+        setTimeout(() => setRedirect(true), 2000);
+
+
+
+
       })
       .catch((error) => {
+        let msg = (
+          <p className={`${styles.StatusMsg} ${styles.Err}`}> There was a problem please try again  </p>
+        )
+        setStatus(msg)
+
+
+
         console.log(error)
       })
   }
-  return (
+  return redirect ? (
+    <Redirect to={"/oproject/" + props.match.params.id} />
+  ) : (
     <div className={styles.Wrapper}>
       <p className={styles.Title}>Contact your team</p>
+      <p className={styles.Message}>This email will be send to the following applicants</p>
+      <div className={styles.UsersContainer}>
+        {applicants.map((applicant) => {
+
+
+          return (
+            <div className={styles.User}>
+              <p className={styles.Name} > {applicant.fullname}</p>
+              <p className={styles.Username} >@{applicant.username}</p>
+            </div>
+          )
+
+
+        })}
+
+      </div>
+
       <div className={styles.InputLabelContainer}>
         <label className={styles.Label}>Subject</label>
         <input name="subject" onChange={handleOnChange} className={styles.Input} />
@@ -47,6 +91,7 @@ const EmailTest = (props) => {
       <div className={styles.ButtonContainer}>
         <button onClick={sendEmail} className={styles.Button}> Send email</button>
       </div>
+      {status}
     </div>
   );
 }
